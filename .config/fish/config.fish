f4b3b94b686811bb7bd80bf3eedc5034da59e855
mpppk <niboshiporipori@gmail.com>
2018-04-07 12:35:18 +0900

    Add config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,3 @@
+set -x GOROOT /usr/local/go
+set -x GOPATH ~/go
+set -x PATH $HOME/go/bin $GOROOT/bin $PATH
--- deploy.fish
+++ deploy.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/fish
 
-set DOT_FILES .config/fish .gitconfig
+set DOT_FILES .config/fish/functions .config/fish/config.fish .gitconfig
 
 for file in $DOT_FILES
   ln -sf $HOME/dotfiles/$file $HOME/$file
