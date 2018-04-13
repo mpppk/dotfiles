22de807654cff1d27cf530a9d048f12a08616b1e
mpppk <niboshiporipori@gmail.com>
2018-04-13 19:08:04 +0900

    Add fishfile

--- deploy.fish
+++ deploy.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/fish
 
-set DOT_FILES .config/fish/functions .config/fish/config.fish .gitconfig
+set DOT_FILES .config/fish/functions .config/fish/config.fish .config/fish/fishfile .gitconfig
 
 for file in $DOT_FILES
   ln -sf $HOME/dotfiles/$file $HOME/$file
