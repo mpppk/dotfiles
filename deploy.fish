164b9a015197db4fde851a950dfa94cb6ff8d2e5
yuki <niboshiporipori@gmail.com>
2018-07-23 23:20:17 +0900

    Add tmux settings

--- deploy.fish
+++ deploy.fish
@@ -1,6 +1,5 @@
-#!/usr/bin/fish
-
-set DOT_FILES .config/fish/functions .config/fish/config.fish .config/fish/fishfile .gitconfig
+#!/usr/local/bin/fish
+set DOT_FILES .config/fish/functions .config/fish/config.fish .config/fish/fishfile .gitconfig .tmux.conf
 
 for file in $DOT_FILES
   ln -sf $HOME/.ghq/github.com/mpppk/dotfiles/$file $HOME/$file
