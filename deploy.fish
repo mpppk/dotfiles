729f784131bdfc0a298b0d2ca00a2c6813866a34
mpppk <niboshiporipori@gmail.com>
2018-08-13 12:30:01 +0900

    Update deploy.fish for tmuxp

--- deploy.fish
+++ deploy.fish
@@ -1,5 +1,7 @@
 #!/usr/local/bin/fish
-set DOT_FILES .config/fish/functions .config/fish/config.fish .config/fish/fishfile .gitconfig .tmux.conf
+set DOT_FILES .config/fish/functions .config/fish/config.fish .config/fish/fishfile .gitconfig .tmux.conf .tmuxp/d.yaml
+
+mkdir -p $HOME/.tmuxp
 
 for file in $DOT_FILES
   ln -sf $HOME/.ghq/github.com/mpppk/dotfiles/$file $HOME/$file
