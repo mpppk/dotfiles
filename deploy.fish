b8c5cd17b5cdedae993e15436175cac87410b48d
mpppk <niboshiporipori@gmail.com>
2018-08-20 17:08:02 +0900

    Add cobra setting

--- deploy.fish
+++ deploy.fish
@@ -1,9 +1,10 @@
 #!/usr/local/bin/fish
-set DOT_FILES .config/fish/functions .config/fish/config.fish .config/fish/fishfile .gitconfig .tmux.conf .tmuxp/d.yaml
+set DOT_FILES .config/fish/functions .config/fish/config.fish .config/fish/fishfile .gitconfig .tmux.conf .tmuxp/d.yaml .atom/package.yaml .cobra.yaml
 
 mkdir -p $HOME/.tmuxp
 mkdir -p $HOME/.config/fish
 mkdir -p $HOME/.credentials/gcp
+mkdir -p $HOME/.atom
 
 for file in $DOT_FILES
   ln -sf $HOME/.ghq/github.com/mpppk/dotfiles/$file $HOME/$file
