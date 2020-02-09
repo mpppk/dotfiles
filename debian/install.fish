bf4ee3509c1113900efe354616a04ca88a6305b8
niboshi <niboshiporipori@gmail.com>
2020-02-09 16:47:51 +0900

    Add fish nvm and yarn (#7)
    * Add fish-nvm
    
    * Add yarn

--- debian/install.fish
+++ debian/install.fish
@@ -10,3 +10,9 @@ fish $DOTPATH/debian/install-go.fish
 source $HOME/.config/fish/config.fish
 fish $DOTPATH/install-go-tools.sh
 fisher
+
+# install node and yarn
+nvm use lts
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn -y
