a65b637933aefa7e41c57e6acd0b3db3bea4f7d7
mpppk <niboshiporipori@gmail.com>
2020-02-11 22:38:35 +0900

    Redeploy after fish installing

--- debian/install.fish
+++ debian/install.fish
@@ -4,6 +4,7 @@ set DOTPATH $HOME/dotfiles
 
 mkdir -p $HOME/.config/fish/functions
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+bash $DOTPATH/deploy.sh
 source $HOME/.config/fish/config.fish
 fisher
 
