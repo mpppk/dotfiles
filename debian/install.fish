f486e5755662d081d3f2db4a47b1143bdf72b1b4
mpppk <niboshiporipori@gmail.com>
2020-02-15 10:50:03 +0000

    Update

--- debian/install.fish
+++ debian/install.fish
@@ -2,7 +2,7 @@
 
 mkdir -p $HOME/.config/fish/functions
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-bash $argv[1]/deploy.sh $DOTPATH
+bash $argv[1]/deploy.sh $argv[1]
 source $HOME/.config/fish/config.fish
 fisher
 
