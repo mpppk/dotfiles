1a7483341d9e34ec1cb765b3821db76bb47546af
mpppk <niboshiporipori@gmail.com>
2020-02-15 23:39:01 +0900

    Add mac settings

---
+++ mac/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "You must pass just one argument to $0 as DOTPATH" 1>&2
+  exit 1
+fi
+DOTPATH=$1
+
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew bundle --file $DOTPATH/Brewfile
+
+$DOTPATH/install-go-tools.sh
+
+mkdir -p $HOME/.config/fish
+fish $DOTPATH/install.fish $DOTPATH
