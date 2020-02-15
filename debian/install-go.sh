6bee0c159abaee8c80ac9a42c593af736c39687c
mpppk <niboshiporipori@gmail.com>
2020-02-15 09:15:51 +0000

    Reduce go installation log

--- debian/install-go.sh
+++ debian/install-go.sh
@@ -2,6 +2,6 @@
 
 GOTAR=go1.13.5.linux-amd64.tar.gz
 
-wget https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
-sudo tar -xvf $GOTAR -C /usr/local
+wget -nv https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
+sudo tar -xf $GOTAR -C /usr/local
 rm $GOTAR
