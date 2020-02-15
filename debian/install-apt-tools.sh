935826595e902dd9b36ae7906daed6ea3e24a04a
mpppk <niboshiporipori@gmail.com>
2020-02-15 04:53:20 +0000

    Fix yarn installation on ubuntu

--- debian/install-apt-tools.sh
+++ debian/install-apt-tools.sh
@@ -1,10 +1,18 @@
 # yarn setup
+
+# Remove cmdtest if exist. See https://github.com/yarnpkg/yarn/issues/2821
+dpkg -s cmdtest &> /dev/null
+if [ $? -eq 0 ]; then
+    sudo apt remove -y cmdtest
+fi
+
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 
 # fish setup
 wget -nv https://download.opensuse.org/repositories/shells:fish:release:3/Debian_9.0/Release.key -O Release.key
 sudo apt-key add - < Release.key
 
+sudo apt-get install -y apt-transport-https
 sudo apt-get update
 sudo apt-get install -y git tig tmux yarn fish
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
