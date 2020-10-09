6b91d0d94b3805377a657f25104daac760391a6f
niboshi <niboshiporipori@gmail.com>
2020-10-09 23:10:09 +0900

    Upgrade Go to 1.15.2

--- debian/install-go.sh
+++ debian/install-go.sh
@@ -2,6 +2,6 @@
 
 GOTAR=go1.15.2.linux-amd64.tar.gz
 
-wget -nv https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
+wget -nv https://dl.google.com/go/go1.15.2.linux-amd64.tar.gz
 sudo tar -xf $GOTAR -C /usr/local
 rm $GOTAR
