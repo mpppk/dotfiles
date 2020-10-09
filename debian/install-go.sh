2e64a77283df60df88e57109fd1f103aadfed8bb
niboshi <niboshiporipori@gmail.com>
2020-10-09 23:03:31 +0900

    Update install-go.sh

--- debian/install-go.sh
+++ debian/install-go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GOTAR=go1.13.5.linux-amd64.tar.gz
+GOTAR=go1.15.2.linux-amd64.tar.gz
 
 wget -nv https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
 sudo tar -xf $GOTAR -C /usr/local
