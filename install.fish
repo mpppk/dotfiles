3a762c5ab3766e5a6c67df4e158c1bf209391219
mpppk <niboshiporipori@gmail.com>
2022-03-21 06:02:13 +0000

    fisherがインストールされてない時だけインストールする

--- install.fish
+++ install.fish
@@ -1,7 +1,11 @@
 #!/usr/bin/fish
 
 mkdir -p $HOME/.config/fish/functions
-curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 bash $argv[1]/deploy.sh $argv[1]
+if type -q fisher
+    echo "fisher installation is skipped. (already installed)"
+else
+    curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+end
 source $HOME/.config/fish/config.fish
 fisher update
