ed90ab76b4e7c11295b2faa082e17018e9b083b8
mpppk <niboshiporipori@gmail.com>
2023-08-11 17:33:44 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,14 @@
+nvm use lts
+op completion fish | source
 set -U FZF_LEGACY_KEYBINDINGS 0
-set -x GOOGLE_APPLICATION_CREDENTIALS $HOME/.credentials/gcp/project.json
 set -x GOROOT /usr/local/go
 set -x GOPATH $HOME/go
-set -x PATH $GOPATH/bin $GOROOT/bin (yarn global bin) $PATH
+set -x PATH /opt/homebrew/opt/libxml2/bin /opt/homebrew/opt/libxslt/bin $GOPATH/bin $GOROOT/bin $HOME/.cargo/bin /opt/homebrew/bin $PATH
 set -x EDITOR vim
+fish_add_path /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
+
+# https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke
+set -x USE_GKE_GCLOUD_AUTH_PLUGIN True
+
+# op inject -i .config/fish/config.fish -o .config/fish/config.fish
+set -x NODE_AUTH_TOKEN {{ op://Personal/GitHub Classic PAT general/token }}
